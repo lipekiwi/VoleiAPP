6deb63dfc40d5cc65cd2c9d7ab76ab2b104e402a
Filipe Correia Santos <lipecorreiasantos@gmail.com>
2025-11-13 09:28:01 -0300

    novo service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,6 @@
 
-const CACHE_NAME = 'volei-smart-cache-v1';
-const FILES_TO_CACHE = [
-  './index.html',
-  './app.css',
-  './app.js',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(caches.keys().then((keys) => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); }))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (evt) => {
-  evt.respondWith(caches.match(evt.request).then((res) => res || fetch(evt.request)));
-});
+const CACHE_NAME = 'volei-app-v2-cache-v1';
+const FILES = ['./index.html','./app.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install', evt => { evt.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES))); self.skipWaiting(); });
+self.addEventListener('activate', evt => { evt.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k=> k !== CACHE_NAME ? caches.delete(k) : Promise.resolve())))); self.clients.claim(); });
+self.addEventListener('fetch', evt => { evt.respondWith(caches.match(evt.request).then(res => res || fetch(evt.request))); });
